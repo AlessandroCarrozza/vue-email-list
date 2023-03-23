93720f8e4c719acf046b7995ed7c7cba62fb7c0c
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-03-23 16:35:56 +0100

    Progetto completato

--- js/main.js
+++ js/main.js
@@ -3,7 +3,18 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        emails: [],
+        randomEmail: "",
       }
+    },
+    
+    mounted() {
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(resp => {
+            this.randomEmail = resp.data.response;
+            this.emails.push(this.randomEmail);
+            });          
+        }
     }
   }).mount('#app')
